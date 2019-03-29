d54d3ee277f514a230fe2270ba7c14078757d722
hangting <hangting@umich.edu>
2019-03-28 21:41:37 -0400

    add cnn

--- Dqn.py
+++ Dqn.py
@@ -1,31 +1,106 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from ReplayMemory import *
+import math
+import random
+
+BATCH_SIZE = 128
+GAMMA = 0.999
+TARGET_UPDATE = 10
+
 
 class DQN(nn.Module):
 
-    def __init__(self, h, w):
+    def __init__(self, n_features, n_actions):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.fc1 = nn.Linear(n_features, 50)
+        self.fc2 = nn.Linear(50, 70)
+        self.fc3 = nn.Linear(70, n_actions)
+        self.relu = nn.ReLU()
+        self.conv = nn.Sequential(
+            # Stage 1
+            nn.Conv2d(4, 8, 2, padding=1),
+            nn.ReLU(),
+            # nn.MaxPool2d(2, stride=2),
+            # Stage 2
+            nn.Conv2d(8, 16, 2, padding=1),
+            nn.ReLU()
+            # nn.MaxPool2d(2, stride=2)
+        )
+        self.fc4 = nn.Linear(16*3*3, 9)
 
-        # Number of Linear input connections depends on output of conv2d layers
-        # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
-        self.head = nn.Linear(linear_input_size, 2) # 448 or 512
+    def forward(self, x, y, z):
+        # x: open requests batch_size x 9
+        # y: available vehicles batch_size x 9
+        # z: history requests batch_size x 4 x 3 x 3
+        output = self.conv(z)
+        output = self.fc4(output.view(x.size(0), -1))
+        output = torch.cat((x, y, output), 1)
+        output = self.relu(self.fc1(output))
+        output = self.relu(self.fc2(output))
+        output = self.fc3(output)
+        return output
 
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+
+def optimize_model(memory, policy_net, target_net, optimizer, device):
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation). This converts batch-array of Transitions
+    # to Transition of batch-arrays.
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.uint8)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+
+def select_action(state, policy_net, eps_end, eps_start, eps_decay, steps_done, n_actions, device):
+    sample = random.random()
+    eps_threshold = eps_end + (eps_start - eps_end) * \
+        math.exp(-1. * steps_done / eps_decay)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
+
+
+
