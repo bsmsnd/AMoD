02f8bed3bc08eae9a647a85d8f60c6fe561ae53d
Hui Sun <huisunum@umich.edu>
2019-04-06 15:03:58 -0400

    Update simulator.py
    Add plot

--- simulator.py
+++ simulator.py
@@ -6,33 +6,41 @@ Created on Wed Apr  3 13:48:42 2019
 """
 
 from constant import NUMBER_OF_VEHICLES
-import DispatchingLogic_demo 
+from DispatchingLogic import DispatchingLogic 
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 from utils.RoboTaxiStatus import RoboTaxiStatus
 import sys
+import matplotlib.pyplot as plt
+import time 
 
 
 # Initialize map
 # map 0.05 degree ~ 5.5km 
-lon = [0.0, 0.05]  # longitude range of the map 
+lon = [0.0, 0.1]  # longitude range of the map 
 lat = [0.0, 0.05]  # latitude range of the map
 bottomLeft = [lon[0], lat[0]]  
 topRight = [lon[1], lat[1]]
 
 # Initialize request
-std_num_request = 0.5  # variance for new request per 10 second
+std_num_request = 0.3  # variance for new request per 10 second
 num_request = 0  # count the total number of request   
 request_dic = {}  # save all the information about the request
-req = [] # list of openrequest
+# all the index of request that have been responsed   
+# but the vehicle is drivingtocustome
+request_wait = []  
+# list of openrequest
+req = [] 
 
 # Initialize vehicle speed
-speed_initial = 40 # km/h
+speed_initial = 30 # km/h
 speed = speed_initial/(3600 * 111.3196)
 
-#
-time = 0
+# constant for plot 
+time_p = 0
+plot_period = 10
+pause_time = 0.01
 
 
 class vehicle:
@@ -41,6 +49,7 @@ class vehicle:
         self.status = RoboTaxiStatus('STAY')
         self.destination = [0., 0.]
         self.destination_custome = [0., 0.]
+        self.requestID = -1
     
     def pick_up(self, req_infor):
         if self.status is RoboTaxiStatus.DRIVEWITHCUSTOMER:
@@ -49,6 +58,7 @@ class vehicle:
         self.status = RoboTaxiStatus('DRIVETOCUSTOMER')
         self.destination = req_infor[2]
         self.destination_custome = req_infor[3]
+        self.requestID = req_infor[0]
     
     def rebalance(self, dest):
         if self.status is RoboTaxiStatus.DRIVEWITHCUSTOMER:
@@ -56,6 +66,8 @@ class vehicle:
             sys.exit(1)
         self.status = RoboTaxiStatus('REBALANCEDRIVE')
         self.destination = dest
+        self.requestID = -1
+        
     def state(self): return self.status
 
 fleet = [vehicle() for _ in range(NUMBER_OF_VEHICLES)]    
@@ -81,19 +93,18 @@ def generate_request():
     global num_request
     global request_dic
     global req
-    global time
+    global time_p
     num_b = abs(int(round(np.random.normal(0,std_num_request))))
     if num_b == 0: return
     for i in range(num_b):
         ori_location = [random.uniform(lon[0], lon[1]), random.uniform(lat[0], lat[1])]
         dest_location = [random.uniform(lon[0], lon[1]), random.uniform(lat[0], lat[1])]
-        req_time = np.random.uniform(time-10, time)
+        req_time = np.random.uniform(time_p-10, time_p)
         req_temp = [num_request, req_time, ori_location, dest_location]
         req.append(req_temp)
         request_dic[num_request] = req_temp
         num_request += 1
     return
-
 
 
 
@@ -106,6 +117,7 @@ def fleet_update(action):
     global fleet
     global speed
     global req
+    global request_wait
     pickup, rebalance = action[0], action[1]
     delete_dic = {}
     # update vehicle state for pick up
@@ -113,7 +125,7 @@ def fleet_update(action):
         vehicle_ID, request_ID = pick[0], pick[1]
         fleet[vehicle_ID].pick_up(request_dic[request_ID])
         delete_dic[request_ID] = 1
-    
+        request_wait.append(request_ID)
     # delete these open request which has been resolved  
     index = 0
     while True:
@@ -135,22 +147,24 @@ def fleet_update(action):
         print(veh.status)
         if veh.status is RoboTaxiStatus.STAY: 
             continue
-        elif (veh.state() is RoboTaxiStatus.DRIVETOCUSTOMER or 
-              veh.state() is RoboTaxiStatus.DRIVEWITHCUSTOMER 
-              or veh.state() is RoboTaxiStatus.REBALANCEDRIVE):
+        elif (veh.status is RoboTaxiStatus.DRIVETOCUSTOMER or 
+              veh.status is RoboTaxiStatus.DRIVEWITHCUSTOMER 
+              or veh.status is RoboTaxiStatus.REBALANCEDRIVE):
             pre_time = cal_time(veh.loc, veh.destination)
             if pre_time < 10:  
             # there is a state change in the 10 second
-                if (veh.state() is RoboTaxiStatus.DRIVEWITHCUSTOMER or 
-                    veh.state() is RoboTaxiStatus.REBALANCEDRIVE):
+                if (veh.status is RoboTaxiStatus.DRIVEWITHCUSTOMER or 
+                    veh.status is RoboTaxiStatus.REBALANCEDRIVE):
                     fleet[i].status = RoboTaxiStatus.STAY
                     fleet[i].loc = veh.destination
                     continue
                 else:
+                    # the state is changing from drivetocustome to drivewithcustome
                     loc, des = veh.destination, veh.destination_custome
                     ratio = (10-pre_time)*speed/cal_dis(loc, des)
                     fleet[i].status = RoboTaxiStatus.DRIVEWITHCUSTOMER
                     fleet[i].destination = veh.destination_custome
+                    request_wait.remove(veh.requestID)
             else:
             # there is no state change in the 10 second
                     loc, des = veh.loc, veh.destination
@@ -165,22 +179,53 @@ def fleet_update(action):
 
 def plot():
     ###########Generate plot for the system################
+    # '*' means request, large red * : open request   
+    # small blue *: request are responsed
+    # 'o' means vehicle
+    # green 'o' : stay or rebalance 
+    # magenta 'o': drive to custome
+    # red 'o': drive with custome
     global fleet
-    pass
-
+    global req
+    global request_dic
+    global request_wait
+    global pause_time
+    plt.clf()
+    for i in range(len(req)):
+        plt.plot(req[i][2][0], req[i][2][1], marker='*', markersize=15, color="r")
+    for i in range(len(request_wait)):
+        req_temp = request_dic[request_wait[i]][2]
+        plt.plot(req_temp[0], req_temp[1], marker='*', markersize=10, color="b")
+        
+    for i in range(len(fleet)):
+        if fleet[i].status is RoboTaxiStatus.REBALANCEDRIVE:
+            plt.plot(fleet[i].loc[0], fleet[i].loc[1], marker='o', markersize=6, color="y")
+        elif fleet[i].status is RoboTaxiStatus.STAY:
+            plt.plot(fleet[i].loc[0], fleet[i].loc[1], marker='o', markersize=6, color="y")
+        elif fleet[i].status is RoboTaxiStatus.DRIVETOCUSTOMER:
+            plt.plot(fleet[i].loc[0], fleet[i].loc[1], marker='o', markersize=6, color="m")
+        elif fleet[i].status is RoboTaxiStatus.DRIVEWITHCUSTOMER:
+            plt.plot(fleet[i].loc[0], fleet[i].loc[1], marker='o', markersize=6, color="r")
+    plt.axis([lon[0], lon[1], lat[0], lat[1]])
+    plt.pause(pause_time)
+    
 
 if __name__ == "__main__":       
-    dispatch = DispatchingLogic_demo.DispatchingLogic(bottomLeft, topRight)
+    dispatch = DispatchingLogic(bottomLeft, topRight)
+    plt.ion()
+    plt.figure(1)
+    
     while True:
-        time += 10
+        time_p += 10
         state_vehicle = [[i, fleet[i].loc, fleet[i].state(), 1] for i in range(NUMBER_OF_VEHICLES)]
         generate_request()
 #        print(state_vehicle)
-        action = dispatch.of([time, state_vehicle, req, [0,0,0]])
+        action = dispatch.of([time_p, state_vehicle, req, [0,0,0]])
 #        print(action)
         fleet_update(action)
+        if time_p % plot_period == 0:
+            plot()
             
-        
         
         
         
